eded697fb8af83148ca2391389b2065d54e178d7
Jullian Sabando <166296896+jlnmx@users.noreply.github.com>
2024-07-02 22:57:44 +0800

    Update MainMenu.java

--- SMSFINAL/src/sms2/MainMenu.java
+++ SMSFINAL/src/sms2/MainMenu.java
@@ -6,39 +6,50 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainMenu extends JFrame implements ActionListener {
-    private JLabel lblTitle, lblQuestion, lblAlreadyHaveAccount;
+    private JLabel lblTitle, lblQuestion, lblSignIn;
     private JButton btnStudent, btnAdmin, btnSignIn;
 
     public MainMenu() {
         setLayout(null);
+        
+        getContentPane().setBackground(new Color(245, 245, 220));
 
         lblTitle = new JLabel("Student Management System");
         lblTitle.setFont(new Font("Bell MT", Font.BOLD, 30));
         lblTitle.setBounds(110, 50, 400, 60);
+        lblTitle.setForeground(new Color(128, 0, 0));
 
         lblQuestion = new JLabel("Are You A?");
         lblQuestion.setFont(new Font("Bell MT", Font.BOLD, 20));
         lblQuestion.setBounds(250, 150, 200, 30);
-
+        lblQuestion.setForeground(new Color(128, 0, 0));
+        
         btnStudent = new JButton("Student");
         btnStudent.setBounds(150, 200, 100, 50);
+        btnStudent.setBackground(new Color(128, 0, 0));
+        btnStudent.setForeground(Color.WHITE);
 
         btnAdmin = new JButton("Admin");
         btnAdmin.setBounds(350, 200, 100, 50);
+        btnAdmin.setBackground(new Color(128, 0, 0));
+        btnAdmin.setForeground(Color.WHITE);
 
-        lblAlreadyHaveAccount = new JLabel("Already have an account?");
-        lblAlreadyHaveAccount.setFont(new Font("Arial", Font.PLAIN, 12));
-        lblAlreadyHaveAccount.setBounds(320, 330, 150, 30);
+        lblSignIn = new JLabel("Already have an account?");
+        lblSignIn.setFont(new Font("Arial", Font.PLAIN, 12));
+        lblSignIn.setBounds(320, 330, 150, 30);
+        lblSignIn.setForeground(new Color(128, 0, 0));
 
         btnSignIn = new JButton("Sign In");
         btnSignIn.setFont(new Font("Arial", Font.PLAIN, 12));
         btnSignIn.setBounds(470, 330, 80, 30);
+        btnSignIn.setBackground(new Color(128, 0, 0));
+        btnSignIn.setForeground(Color.WHITE);
 
         add(lblTitle);
         add(lblQuestion);
         add(btnStudent);
         add(btnAdmin);
-        add(lblAlreadyHaveAccount);
+        add(lblSignIn);
         add(btnSignIn);
 
         setTitle("Student Management System");
