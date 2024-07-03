2b56d570badbb36de284cf80e4d0f4b8cf13693d
Jullian Sabando <166296896+jlnmx@users.noreply.github.com>
2024-07-03 17:01:15 +0800

    Update SMSMAIN.java

--- SMSFINAL/src/sms2/SMSMAIN.java
+++ SMSFINAL/src/sms2/SMSMAIN.java
@@ -5,13 +5,14 @@ import java.awt.EventQueue;
 public class SMSMAIN {
 
     public static void main(String[] args) {
+        // TODO code application logic here
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                MainMenu m = new MainMenu();
+                Homepage m = new Homepage();
                 m.setVisible(true);
             }
         });
     }
-    
+
 }
